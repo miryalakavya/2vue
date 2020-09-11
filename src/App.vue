bf87088d1b0454314605890ca17803a4a89c5acb
dileepkumarkethe <kethedileepkumar1@gmail.com>
2020-09-11 14:56:31 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" id="input" v-model="msg" placeholder="Enter something"/> <br><br><br>
+    <button id="click" type="button" @click="click">Submit</button><br><br>
+    <p id="input"> {{msg | toUpper }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+   return {
+     msg: ''
+   }
+  },
+  methods: {
+    click() {
+      this.msg = this.msg.toUpperCase();
+    }
   }
 }
+  
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    margin-left: 36%;
+    margin-top: 10%;
+  }
+  #click {
+    margin-left: 16%;
+    cursor: pointer;
+    background-color: black;
+    color: white;
+    width: 80px;
+    height: 25px;
+    border: 2px dotted red;
+    border-radius: 10px;
+  }
+  #input {
+    border: 2.5px solid skyblue;
+    border-radius: 20px;
+    width: 350px;
+    height: 30px;
+  }
+  #input:hover {
+    border: 2.5px solid red;
+  }
 </style>
